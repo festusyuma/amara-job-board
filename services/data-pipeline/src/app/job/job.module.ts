84903d76cb468d39ce7c6eba50c3c0373b925c53
Festus <festusyuma@gmail.com>
2025-08-17 02:14:57 +0100

    move db into lib, created chat tables, grant permissions

--- services/data-pipeline/src/app/job/job.module.ts
+++ services/data-pipeline/src/app/job/job.module.ts
@@ -1,8 +1,10 @@
+import { DbModule } from '@amara/db';
 import { Module } from '@nestjs/common';
 
 import { JobService } from './job.service';
 
 @Module({
+  imports: [DbModule],
   providers: [JobService],
   exports: [JobService]
 })
--- services/data-pipeline/src/app/message.module.ts
+++ services/data-pipeline/src/app/message.module.ts
@@ -2,14 +2,13 @@ import { UtilModule as BaseUtilModule } from '@amara/helpers/util';
 import { Module } from '@nestjs/common';
 
 import { ChatModule } from './chat/chat.module';
-import { DbModule } from './db/db.module';
 import { JobModule } from './job/job.module';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 import { UtilModule } from './util/util.module';
 
 @Module({
-  imports: [BaseUtilModule, UtilModule, JobModule, DbModule, ChatModule],
+  imports: [BaseUtilModule, UtilModule, JobModule, ChatModule],
   controllers: [MessageController],
   providers: [MessageService],
 })
